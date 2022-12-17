16df74612be495be3d7bcc1494f66609bd1f5d5b
Mohit Pardhi <mpardhi11@gmail.com>
2022-12-17 23:34:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./components/Hearder";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <h1>Hello World</h1>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
